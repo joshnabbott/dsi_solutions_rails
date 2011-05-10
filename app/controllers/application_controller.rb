03dc7475ad5e64db88413e458c54b0bd67e84a07
Josh N. Abbott <joshnabbott@gmail.com>
2011-05-09 19:44:56 -0600

    Adding before filter to find and set @distributor.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :authenticate_user!
+  prepend_before_filter :authenticate_user!
+  before_filter :find_distributor
+
+protected
+  def find_distributor
+    @distributor = Distributor.where(:id => params[:distributor_id]).first
+  end
 end
